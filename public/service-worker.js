b637ff89ca0675956ee6a301d10abc12ab5c2300
Sarah Vivoli <snvivoli@gmail.com>
2020-03-06 10:43:54 -0800

    activate function

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,18 @@ self.addEventListener("install", function(event) {
     );
     self.skipWaiting();
 });
+
+// activate
+self.addEventListener("activate", function(event) {
+    event.waitUntil(caches.keys().then(keyList => {
+        return Promise.all( 
+            keyList.map( key => {
+            if ( key !== CACHE_NAME && key!== DATA_CACHE_NAME ) {
+                console.log('Removing old key: ', key);
+                return caches.delete(key);
+            };
+        }));
+    }));
+    self.clients.claim();
+});
+
