2616cc3f57e5185ecf0197dad7b60bff73dab734
Sarah Vivoli <snvivoli@gmail.com>
2020-03-06 12:17:08 -0800

    fetch function updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.webmanifest',
-    '/style.css',
+    '/styles.css',
     '/index.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
@@ -35,18 +35,32 @@ self.addEventListener("activate", function(event) {
 });
 
 // fetch
-self.addEventListener("fetch", function(event) {
-    if ( event.request.url.includes('/api') ) {
-        event.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(event.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(event.response.url, response.clone());
-                    }
-                    return response;
-                })
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
             })
-        );
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
     };
-});
+    
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    });
