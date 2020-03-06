037d56e1aae9c5c892839784de81ef41285e943f
Sarah Vivoli <snvivoli@gmail.com>
2020-03-06 10:40:37 -0800

    service worker files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/style.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
+
+  
