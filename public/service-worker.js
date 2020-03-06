f440bfa1471a5ecab248c1b42da58c0a6f17913e
Sarah Vivoli <snvivoli@gmail.com>
2020-03-06 10:43:26 -0800

    install function

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,11 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// install
+self.addEventListener("install", function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
